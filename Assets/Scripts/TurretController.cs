6e2b9323623760744e0c68f048d9959c52a315e5
Jade <jade16work@gmail.com>
2026-09-07 12:07:41 +0900

    [Feat] 터렛 구현

---
+++ Assets/Scripts/TurretController.cs
@@ -0,0 +1,136 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.PackageManager;
+using UnityEngine;
+
+public class TurretController : MonoBehaviour
+{
+    [SerializeField] private float _rotateSpeed;
+    [SerializeField] private float _cooldown;
+    [SerializeField] private Transform _headTransform;
+    [SerializeField] private Transform _muzzlePoint;
+    
+    [Header("Bullet")]
+    [SerializeField] private BulletController _bulletPrefab;
+    [SerializeField] private int _bulletDamage;
+    [SerializeField] private float _bulletSpeed;
+    [SerializeField] private float _bulletDestroyDelay;
+    
+    private float _currentCooldown;
+    private Transform _playerTransform;
+    private bool _isPlayerInTrigger { get { return _playerTransform != null; } }
+    private bool _isPlayerInSight = false;
+    private bool _isReadyToFire { get { return _currentCooldown >= _cooldown; } }
+    private SphereCollider _sphereCollider;
+    
+    // Unity Event Method -----------------------
+    private void Awake() => CacheComponents();
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _playerTransform = other.transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _playerTransform = null;
+        }
+    }
+
+    private void Update()
+    {
+        UpdateCurrentCooldown();
+        RayShotToPlayer();
+        Rotate();
+        Fire();
+    }
+    // --------------------------------------------
+    
+    
+    private void CacheComponents()
+    {
+        _sphereCollider = GetComponent<SphereCollider>();
+    }
+
+    private void Fire()
+    {
+        if (!_isPlayerInSight || !_isPlayerInTrigger) return;
+
+        Vector3 look = new Vector3(
+            _playerTransform.position.x,
+            _headTransform.position.y,
+            _playerTransform.position.z
+            );
+        
+        _headTransform.LookAt(look);
+
+        if (!_isReadyToFire) return;
+        
+        SpawnBullet();
+        _currentCooldown = 0f;
+    }
+
+    private void UpdateCurrentCooldown()
+    {
+        if (_isReadyToFire) return;
+        
+        _currentCooldown += Time.deltaTime;
+    }
+
+    private void SpawnBullet()
+    {
+        BulletController bullet = Instantiate(
+            _bulletPrefab, 
+            _muzzlePoint.position, 
+            _muzzlePoint.rotation
+            );
+        
+        bullet.SetData(_bulletDamage, _bulletSpeed, _bulletDestroyDelay);
+    }
+
+    private void Rotate()
+    {
+        if (_isPlayerInSight) return;
+        
+        _headTransform.Rotate(Vector3.up, _rotateSpeed * Time.deltaTime);
+    }
+
+    private void RayShotToPlayer()
+    {
+        _isPlayerInSight = false;
+        if (!_isPlayerInTrigger) return;
+
+        Vector3 from = new Vector3(
+            transform.position.x,
+            transform.position.y + _muzzlePoint.position.y / 2,
+            transform.position.z
+            );
+        
+        Vector3 to = new Vector3(
+            _playerTransform.position.x,
+            _playerTransform.position.y + _muzzlePoint.position.y / 2,
+            _playerTransform.position.z
+            );
+        
+        Ray ray = new Ray(from, (to - from).normalized);
+        RaycastHit hit;
+
+        // 대안 :  발사 높이와 감지 높이를 다르게 해서 우회
+        // TODO: LayerMask 배운 후 리팩토링
+        
+        if (Physics.Raycast(ray, out hit, _sphereCollider.radius))
+        {
+            // 레이어마스크 쓰고싶다
+            if (hit.transform.CompareTag("Player"))
+            {
+                _isPlayerInSight = true;
+                Debug.Log("플레이어 감지");
+            }
+        }
+    }
+}
