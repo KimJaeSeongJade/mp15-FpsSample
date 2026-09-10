3769d5ebefb5446f1e788225e90c97d84b242938
Jade <jade16work@gmail.com>
2026-09-10 15:13:53 +0900

    [Feat] Turret Hp Gauge 회전 기능 추가

--- Assets/Scripts/EnemyUIController.cs
+++ Assets/Scripts/EnemyUIController.cs
@@ -6,12 +6,28 @@ using UnityEngine.UI;
 
 public class EnemyUIController : MonoBehaviour
 {
+    [SerializeField] private Transform _gaugeTransform;
     [SerializeField] private Image _gauge;
     [SerializeField] private TextMeshProUGUI _text;
 
+    private Transform _cameraTransform;
+    
+    private void Awake() => CacheComponents();
+    private void LateUpdate() => SetRotation();
+    
     public void RefreshHealthUI(int health, int maxHealth)
     {
         _gauge.fillAmount = health / (float)maxHealth;
         _text.text = $"{health}/{maxHealth}";
+    }
+
+    private void CacheComponents()
+    {
+        _cameraTransform = Camera.main.transform;
+    }
+
+    private void SetRotation()
+    {
+        _gaugeTransform.forward = _cameraTransform.forward;
     }
 }
