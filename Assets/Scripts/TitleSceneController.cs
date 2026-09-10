1f1ebb0ef407c8e9de341e0acf9ed406f0cc0dc9
Jade <jade16work@gmail.com>
2026-09-10 15:02:12 +0900

    [Feat] 타이틀 게임종료 기능 추가

--- Assets/Scripts/TitleSceneController.cs
+++ Assets/Scripts/TitleSceneController.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 public class TitleSceneController : MonoBehaviour
 {
     [SerializeField] private Button _startButton;
+    [SerializeField] private Button _quitButton;
 
     private void OnEnable() => BindButtonEvents();
     private void OnDisable() => UnbindButtonEvents();
@@ -14,15 +15,22 @@ public class TitleSceneController : MonoBehaviour
     private void BindButtonEvents()
     {
         _startButton.onClick.AddListener(LoadGameScene);
+        _quitButton.onClick.AddListener(Quit);
     }
 
     private void UnbindButtonEvents()
     {
         _startButton.onClick.RemoveListener(LoadGameScene);
+        _quitButton.onClick.RemoveListener(Quit);
     }
 
     private void LoadGameScene()
     {
         SceneManager.LoadScene(1);
+    }
+
+    private void Quit()
+    {
+        Application.Quit();
     }
 }
