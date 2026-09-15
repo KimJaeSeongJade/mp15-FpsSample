dd798fc82e42d589da098eb229b38d54d45eeddf
Jade <jade16work@gmail.com>
2026-09-15 14:17:30 +0900

    [Refactor] 플레이어 Interactable 반복 감지 코루틴으로 개선

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour, IInteractor
@@ -7,7 +8,10 @@ public class PlayerController : MonoBehaviour, IInteractor
     [SerializeField] private GameManager _gameManager;
     [SerializeField] private Transform _cameraPivot;
     [SerializeField] private float _detectionRange;
-    
+    [SerializeField] private float _tryInteactCooldown;
+
+    private Coroutine _tryInteractRoutine;
+    private WaitForSeconds _waitRetryInteract;
     private PlayerWeapon _weapon;
     private PlayerMovement _movement;
     private Transform _cameraTransform;
@@ -20,15 +24,29 @@ public class PlayerController : MonoBehaviour, IInteractor
     public InputManager PlayerInput => InputManager.Instance;
 
     // -----------------------------------------------
-    private void Awake() => CacheComponents();
-    private void OnEnable() => BindInputAcions();
-    private void Update() => DetectInteractable();
+    private void Awake()
+    {
+        CacheComponents();
+        Init();
+    }
+
+    private void OnEnable()
+    {
+        BindInputAcions();
+        StartDetectInteractable();
+    }
+
     private void LateUpdate()
     {
         SetWeaponTransform();
         SetCameraTransform();
     }
-    private void OnDisable() => UnbindInputAcions();
+
+    private void OnDisable()
+    {
+        UnbindInputAcions();
+        StopDetectInteractable();
+    }
     // -----------------------------------------------
     
     private void BindInputAcions()
@@ -72,34 +90,55 @@ public class PlayerController : MonoBehaviour, IInteractor
             );
     }
 
-    public void DetectInteractable()
+    private void StartDetectInteractable()
     {
-        Ray ray = new Ray(_cameraTransform.position, _cameraTransform.forward);
-        RaycastHit hit;
+        if (_tryInteractRoutine != null) return;
 
-        if (!Physics.Raycast(ray, out hit, _detectionRange))
+        _tryInteractRoutine = StartCoroutine(DetectInteractableRoutine());
+    }
+
+    private void StopDetectInteractable()
+    {
+        if (_tryInteractRoutine == null) return;
+        
+        StopCoroutine(_tryInteractRoutine);
+        _tryInteractRoutine = null;
+    }
+
+
+    private IEnumerator DetectInteractableRoutine()
+    {
+        while (true)
         {
+            yield return _waitRetryInteract;
+            
+            Ray ray = new Ray(_cameraTransform.position, _cameraTransform.forward);
+            RaycastHit hit;
+
+            if (!Physics.Raycast(ray, out hit, _detectionRange))
+            {
+                if (_hasDetectInteractable)
+                {
+                    _targetInteractable.Untargeting();
+                    _targetInteractable = null;
+                }
+
+                continue;
+            }
+
             if (_hasDetectInteractable)
             {
-                _targetInteractable.Untargeting();
-                _targetInteractable = null;
+                if (hit.collider.gameObject == _targetInteractable.GameObject)
+                {
+                    continue; // 같은 Interactable을 계속 주시하고 있는 경우
+                }
             }
 
-            return;
-        }
-
-        if (_hasDetectInteractable)
-        {
-            if (hit.collider.gameObject == _targetInteractable.GameObject)
-            {
-                return; // 같은 Interactable을 계속 주시하고 있는 경우
-            }
-        }
-
-        _targetInteractable?.Untargeting();
-        _targetInteractable = hit.collider.GetComponent<IInteractable>();
+            _targetInteractable?.Untargeting();
+            _targetInteractable = hit.collider.GetComponent<IInteractable>();
         
-        _targetInteractable?.Targeting();
+            _targetInteractable?.Targeting();
+        }
     }
 
     public void TryInteract()
@@ -108,5 +147,10 @@ public class PlayerController : MonoBehaviour, IInteractor
         
         _targetInteractable.Interact(this);
         _targetInteractable = null;
+    }
+
+    private void Init()
+    {
+        _waitRetryInteract = new WaitForSeconds(_tryInteactCooldown);
     }
 }
